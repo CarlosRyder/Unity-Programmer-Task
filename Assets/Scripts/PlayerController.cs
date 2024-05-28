a9f9fa188f98241a688b904fe04e1aadab7aa429
Carlos Palacios <carlos_a.riascos@uao.edu.co>
2024-05-28 12:13:36 -0500

    first animations added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,17 +7,46 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D playerRB;
     [SerializeField]
     private float moveSpeed;
-    // Start is called before the first frame update
+    private Animator animator;
+    private int idSpeedX;
+    private int idSpeedY;
+    private int idHorizontal;
+    private int idVertical;
+
     private void Awake()
     {
         playerRB = GetComponent<Rigidbody2D>();
     }
+    private void Start()
+    {
+        animator = GameObject.Find("Player").GetComponent<Animator>();
+        idSpeedX = Animator.StringToHash("speedX");
+        idSpeedY = Animator.StringToHash("speedY");
+        idHorizontal = Animator.StringToHash("horizontal");
+        idVertical = Animator.StringToHash("vertical");
 
-    // Update is called once per frame
+    }
+
     void Update()
     {
         float inputX = Input.GetAxisRaw("Horizontal") * moveSpeed;
         float inputY = Input.GetAxisRaw("Vertical") * moveSpeed;
         playerRB.velocity = new Vector2(inputX, inputY);
+        animator.SetFloat(idSpeedX, Mathf.Abs(inputX));
+        animator.SetFloat(idSpeedY, Mathf.Abs(inputY));
+        animator.SetFloat(idHorizontal, inputX);
+        animator.SetFloat(idVertical, inputY);
+
+        if (inputX > 0) 
+        {
+           transform.localScale=new Vector3(-0.5f, 0.5f, 0.5f);
+        }
+
+        else if(inputX < 0) 
+        {
+            transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        }
+
     }
+
 }

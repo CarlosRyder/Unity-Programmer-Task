88739f09632bb94f2b6591c646b0bf4bb21410fb
Carlos Palacios <carlos_a.riascos@uao.edu.co>
2024-05-29 08:26:04 -0500

    player and UI changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,21 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D playerRB;
-    [SerializeField]
-    private float moveSpeed;
+    [SerializeField] private float moveSpeed;
     private Animator animator;
     private int idSpeedX;
     private int idSpeedY;
     private int idHorizontal;
     private int idVertical;
+    public GameObject playerWithoutWeapons;
+    public GameObject playerWithHammer;
+    public GameObject playerWithBow;
+    public GameObject playerWithScythe;
+    private GameObject activePlayer; // Referencia al GameObject activo actualmente
+
 
     private void Awake()
     {
@@ -21,14 +24,23 @@ public class PlayerController : MonoBehaviour
 
     private void Start()
     {
-        animator = GameObject.Find("Player").GetComponent<Animator>();
+        GameObject[] playerObjects = { playerWithoutWeapons, playerWithHammer, playerWithBow, playerWithScythe };
+
+        foreach (GameObject playerObject in playerObjects)
+        {
+            if (playerObject.activeSelf)
+            {
+                SetActivePlayer(playerObject);
+                break; // Una vez que se encuentre el jugador activo, salimos del bucle
+            }
+        }
+
         idSpeedX = Animator.StringToHash("speedX");
         idSpeedY = Animator.StringToHash("speedY");
         idHorizontal = Animator.StringToHash("horizontal");
         idVertical = Animator.StringToHash("vertical");
-
-
     }
+
 
     void Update()
     {
@@ -37,23 +49,57 @@ public class PlayerController : MonoBehaviour
 
     private void Move()
     {
-        float inputX = Input.GetAxisRaw("Horizontal") * moveSpeed;
-        float inputY = Input.GetAxisRaw("Vertical") * moveSpeed;
-        playerRB.velocity = new Vector2(inputX, inputY);
-        animator.SetFloat(idSpeedX, Mathf.Abs(inputX));
-        animator.SetFloat(idSpeedY, Mathf.Abs(inputY));
-        animator.SetFloat(idHorizontal, inputX);
-        animator.SetFloat(idVertical, inputY);
-
-        if (inputX > 0)
+        if (activePlayer != null)
         {
-            transform.localScale = new Vector3(-0.4f, 0.4f, 0.4f);
-        }
+            float inputX = Input.GetAxisRaw("Horizontal") * moveSpeed;
+            float inputY = Input.GetAxisRaw("Vertical") * moveSpeed;
+            playerRB.velocity = new Vector2(inputX, inputY);
+            animator.SetFloat(idSpeedX, Mathf.Abs(inputX));
+            animator.SetFloat(idSpeedY, Mathf.Abs(inputY));
+            animator.SetFloat(idHorizontal, inputX);
+            animator.SetFloat(idVertical, inputY);
 
-        else if (inputX < 0)
-        {
-            transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
+            if (inputX > 0)
+            {
+                transform.localScale = new Vector3(-0.4f, 0.4f, 0.4f);
+            }
+            else if (inputX < 0)
+            {
+                transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
+            }
         }
-
     }
- }
+
+    // Método para establecer el jugador activo
+    private void SetActivePlayer(GameObject player)
+    {
+        if (activePlayer != null)
+        {
+            activePlayer.SetActive(false); // Desactivar el jugador anteriormente activo
+        }
+        activePlayer = player;
+        activePlayer.SetActive(true); // Activar el nuevo jugador
+        animator = activePlayer.GetComponent<Animator>(); // Obtener el Animator del nuevo jugador
+    }
+
+    // Métodos para cambiar el jugador activo mediante botones
+    public void SetPlayerWithoutWeaponsActive()
+    {
+        SetActivePlayer(playerWithoutWeapons);
+    }
+
+    public void SetPlayerWithHammerActive()
+    {
+        SetActivePlayer(playerWithHammer);
+    }
+
+    public void SetPlayerWithBowActive()
+    {
+        SetActivePlayer(playerWithBow);
+    }
+
+    public void SetPlayerWithScytheActive()
+    {
+        SetActivePlayer(playerWithScythe);
+    }
+}
